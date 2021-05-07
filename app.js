695e0a25faabec79c51fb9681d24f09b481c8fdb
AfonSir <afonsir17@gmail.com>
2021-05-06 22:08:53 -0300

    feat: parse request body and write content to a file

--- app.js
+++ app.js
@@ -15,9 +15,23 @@ const server = http.createServer((request, response) => {
   }
 
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+
+    request.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+
+      fs.writeFileSync('message.txt', message);
+    });
+
     response.statusCode = 302;
     response.setHeader('Location', '/');
+
     return response.end();
   }
 
